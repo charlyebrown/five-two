75d0f5b2fa38193abfd26a5f292f985b710e3be0
charlyebrown <tran1226@yahoo.com>
2014-08-28 10:19:19 -0400

    added backbars files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require underscore
+//= require backbone
+//= require handlebars
+//= require_tree ./backbone/routers
+//= require_tree ./backbone/models
+//= require_tree ./backbone/collections
+//= require_tree ./backbone/views
+//= require_tree ./templates
 //= require bootstrap-sprockets
 //= require_tree .
