5ab66e9e1f0b7ba5e74d0ee0b3a40b30eb9d2c25
charlyebrown <tran1226@yahoo.com>
2014-08-31 11:16:30 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+# user = CreateAdminService.new.call
+# puts 'CREATED ADMIN USER: ' << user.email
 
 User.create({name: 'Ems', birthday: 'Dec 1, 2000', email: 'ems@em.com', password: '11111111', password_confirmation: '11111111'})
 User.create({name: 'Charlye', birthday: 'Jan 1, 2000', email: 'c@c.com', password: '11111111', password_confirmation: '11111111'})
